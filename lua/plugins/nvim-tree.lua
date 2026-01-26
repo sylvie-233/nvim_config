5029c44ec51b32d9397a9a50dca85c6fcd3d5a07
Sylvie233 <sylvie233@126.com>
2026-01-26 21:31:14 +0800

    feat(plugins/nvim-tree): 补充文件操作快捷键

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,22 +1,55 @@
 return {
     "nvim-tree/nvim-tree.lua",
-    dependencies = {"nvim-tree/nvim-web-devicons"},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("nvim-tree").setup({
+        local nvim_tree = require("nvim-tree")
+        local api = require("nvim-tree.api")
+
+        nvim_tree.setup({
             view = {
-                width = 30
+                width = 30,
+                side = "left",
             },
             renderer = {
-                highlight_git = true
-            }
+                highlight_git = true,
+            },
+            -- 最新版本推荐用 on_attach 配置快捷键
+            on_attach = function(bufnr)
+                local opts = function(desc)
+                    return { desc = desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+                end
+
+                -- 文件操作快捷键
+                vim.keymap.set("n", "a", api.fs.create, opts("Create"))
+                vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
+                vim.keymap.set("n", "d", api.fs.remove, opts("Delete"))
+                vim.keymap.set("n", "R", api.tree.reload, opts("Refresh"))
+                vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+                vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+                vim.keymap.set("n", "h", api.node.open.horizontal, opts("Open: Horizontal Split"))
+                -- 上一级目录
+                vim.keymap.set("n", "-", api.tree.change_root_to_parent, opts("Up"))
+                vim.keymap.set("n", "C", api.tree.change_root_to_node, { desc = "Set root to selected directory" })
+            end,
         })
-        vim.keymap.set('n', '<leader>e', function()
-            require("nvim-tree.api").tree.toggle({
-                focus = true
-            })
-        end, {
-            noremap = true,
-            silent = true
-        })
-    end
+
+        -- 全局快捷键打开/关闭 nvim-tree
+        vim.keymap.set("n", "<leader>e", function()
+            api.tree.toggle({ focus = true })
+        end, { noremap = true, silent = true })
+
+        vim.keymap.set("n", "<leader>fe", function()
+            api.tree.focus()  -- 直接把光标切换到 nvim-tree
+        end, { noremap = true, silent = true })
+
+        -- 切换目录
+        vim.keymap.set("n", "<leader>cd", function()
+            local input = vim.fn.input("Enter path: ", vim.loop.cwd() .. "/", "dir")
+            if vim.fn.isdirectory(input) == 1 then
+                api.tree.change_root(input)
+            else
+                print("Not a valid directory!")
+            end
+        end, { noremap = true, silent = true })
+    end,
 }
