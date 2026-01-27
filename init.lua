995ad04c53c1abe5aae6a18a03b3dbce0e8c4ee0
Sylvie233 <sylvie233@126.com>
2026-01-27 21:14:49 +0800

    feat(option): 添加powershell默认终端配置

--- init.lua
+++ init.lua
@@ -4,10 +4,16 @@ vim.opt.encoding = "utf-8"
 vim.opt.fileencodings = "utf-8,gbk,gb2312,gb18030"
 vim.opt.number = true
 vim.opt.termguicolors = true
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = "unnamedplus" -- 把 Neovim 的 + 寄存器绑定到系统剪贴板（让 " 默认映射到 +）
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.shell = "pwsh" -- 内置终端使用powershell
+vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+vim.opt.shellredir = "2>&1 | Out-File -Encoding UTF8 %s"
+vim.opt.shellpipe  = "2>&1 | Out-File -Encoding UTF8 %s"
+vim.opt.shellquote = ""
+vim.opt.shellxquote = ""
 
 -- 快捷键配置
 vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
